823892cb9f22139596b50aa1f27fff0b79133163
puji rahmadiyano <puji.rahmadiyanto@gmail.com>
2011-06-02 20:00:04 +0700

    first commit

---
+++ src/hitungzakat/HitungZakat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungzakat;
+
+/**
+ *
+ * @author poedjex
+ */
+public class HitungZakat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      FormUtama baru = new FormUtama();
+      baru.setVisible(true);
+    }
+}
